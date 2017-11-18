d5495e0bdc6b2bb29134af95be70c24b5a0e9526
joelee <as955133@gmail.com>
2017-11-18 15:51:00 +0800

    create project

---
+++ MyAlbum/1.FrmDataPhoto.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace MyAlbum
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPhotoType = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.picUploadPicture = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lstPhotoType = new System.Windows.Forms.ListBox();
+            this.lstPhoto = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dtPhotoDate = new System.Windows.Forms.DateTimePicker();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUploadPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 88);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1033, 626);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dtPhotoDate);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.txtPhotoType);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.picUploadPicture);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.txtFileName);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.splitContainer2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1025, 594);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "相片";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(792, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 19);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "PhotoType";
+            // 
+            // txtPhotoType
+            // 
+            this.txtPhotoType.Location = new System.Drawing.Point(834, 97);
+            this.txtPhotoType.Name = "txtPhotoType";
+            this.txtPhotoType.Size = new System.Drawing.Size(147, 26);
+            this.txtPhotoType.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(899, 440);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 28);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "取消";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(899, 348);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 28);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "讀取";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(899, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 28);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "檔案";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // picUploadPicture
+            // 
+            this.picUploadPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picUploadPicture.Location = new System.Drawing.Point(795, 206);
+            this.picUploadPicture.Name = "picUploadPicture";
+            this.picUploadPicture.Size = new System.Drawing.Size(185, 136);
+            this.picUploadPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUploadPicture.TabIndex = 6;
+            this.picUploadPicture.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(792, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PhotoDate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(792, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PhotoName";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(834, 46);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(147, 26);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(800, 530);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "刪除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Location = new System.Drawing.Point(6, 6);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lstPhotoType);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lstPhoto);
+            this.splitContainer2.Size = new System.Drawing.Size(767, 481);
+            this.splitContainer2.SplitterDistance = 133;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // lstPhotoType
+            // 
+            this.lstPhotoType.DisplayMember = "PhotoType";
+            this.lstPhotoType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstPhotoType.FormattingEnabled = true;
+            this.lstPhotoType.ItemHeight = 19;
+            this.lstPhotoType.Items.AddRange(new object[] {
+            "全部"});
+            this.lstPhotoType.Location = new System.Drawing.Point(0, 0);
+            this.lstPhotoType.Name = "lstPhotoType";
+            this.lstPhotoType.Size = new System.Drawing.Size(133, 481);
+            this.lstPhotoType.TabIndex = 0;
+            this.lstPhotoType.ValueMember = "PhotoType";
+            this.lstPhotoType.SelectedIndexChanged += new System.EventHandler(this.lstPhotoType_SelectedIndexChanged);
+            // 
+            // lstPhoto
+            // 
+            this.lstPhoto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstPhoto.Location = new System.Drawing.Point(0, 0);
+            this.lstPhoto.Name = "lstPhoto";
+            this.lstPhoto.Size = new System.Drawing.Size(630, 481);
+            this.lstPhoto.TabIndex = 0;
+            this.lstPhoto.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(800, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "新增";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Honeydew;
+            this.tabPage1.Controls.Add(this.splitContainer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1025, 594);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tool";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(6, 21);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel2);
+            this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1013, 555);
+            this.splitContainer1.SplitterDistance = 187;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(17, 18);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(786, 520);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(144, 145);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1026, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dtPhotoDate
+            // 
+            this.dtPhotoDate.Location = new System.Drawing.Point(834, 163);
+            this.dtPhotoDate.Name = "dtPhotoDate";
+            this.dtPhotoDate.Size = new System.Drawing.Size(200, 26);
+            this.dtPhotoDate.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 726);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUploadPicture)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox picUploadPicture;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListBox lstPhotoType;
+        private System.Windows.Forms.ListView lstPhoto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPhotoType;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.DateTimePicker dtPhotoDate;
+    }
+}
+
---
+++ MyAlbum/1.FrmDataPhoto.cs
@@ -0,0 +1,156 @@
+﻿using MyAlbum.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace MyAlbum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var cn = new SqlConnection(Settings.Default.DP))
+            using (var adp = new SqlDataAdapter(@"
+                SELECT DISTINCT
+                    PhotoType
+                FROM
+                    PhotoTable;", cn))
+            {
+                var dt = new DataTable();
+
+                adp.Fill(dt);
+
+                var allRow = dt.NewRow();
+                allRow["PhotoType"] = "全部";
+
+                dt.Rows.InsertAt(allRow, 0);
+
+                lstPhotoType.DataSource = dt;
+            }
+        }
+
+        private void lstPhotoType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var photoType = (string)lstPhotoType.SelectedValue;
+
+            var sqlText = string.Empty;
+
+            if (photoType == "全部")
+            {
+                sqlText = @"
+                    SELECT
+                        Id,
+                        Photo
+                    FROM
+                        PhotoTable;";
+            }
+            else
+            {
+                sqlText = @"
+                    SELECT
+                        Id,
+                        Photo
+                    FROM
+                        PhotoTable
+                    WHERE
+                        PhotoType = @type;";
+            }
+
+            using (var cn = new SqlConnection(Settings.Default.DP))
+            using (var adp = new SqlDataAdapter(sqlText, cn))
+            {
+                adp.SelectCommand.Parameters.Add("type", SqlDbType.NVarChar).Value = photoType;
+                var dt = new DataTable();
+
+                adp.Fill(dt);
+
+                var imgList = new ImageList();
+                imgList.ImageSize = new Size(100, 100);
+                lstPhoto.LargeImageList = imgList;
+
+                lstPhoto.Items.Clear();
+                foreach (var row in dt.AsEnumerable())
+                {
+                    var photoBytes = row.Field<byte[]>("Photo");
+                    var img = Image.FromStream(new MemoryStream(photoBytes));
+
+                    var id = row.Field<int>("Id").ToString();
+                    imgList.Images.Add(id, img);
+                    var item = lstPhoto.Items.Add(id);
+                    item.ImageKey = id;
+                }
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                picUploadPicture.Image = Image.FromStream(openFileDialog1.OpenFile());
+                txtFileName.Text = openFileDialog1.SafeFileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var cn = new SqlConnection(Settings.Default.DP))
+            using (var cmd = new SqlCommand(@"
+                INSERT INTO PhotoTable(PhotoDesc, PhotoDate, PhotoType, Photo)
+                VALUES(@fileName, @date, @type, @photo);", cn))
+            {
+                cmd.Parameters.Add("fileName", SqlDbType.NVarChar).Value = txtFileName.Text;
+                cmd.Parameters.Add("date", SqlDbType.DateTime).Value = dtPhotoDate.Value;
+                cmd.Parameters.Add("type", SqlDbType.NVarChar).Value = txtPhotoType.Text;
+
+                using (var ms = new MemoryStream())
+                {
+                    picUploadPicture.Image.Save(ms, ImageFormat.Jpeg);
+
+                    cmd.Parameters.Add("photo", SqlDbType.VarBinary).Value = ms.ToArray();
+                }
+
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var selectedItem = lstPhoto.SelectedItems.Cast<ListViewItem>().First();
+
+            var selectedId = int.Parse(selectedItem.Text);
+            // MessageBox.Show(selectedItem.Text);
+
+            using (var cn = new SqlConnection(Settings.Default.DP))
+            using (var cmd = new SqlCommand(@"
+                DELETE PhotoTable
+                WHERE
+                    Id = @id", cn))
+            {
+                cmd.Parameters.Add("id", SqlDbType.Int).Value = selectedId;
+
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
